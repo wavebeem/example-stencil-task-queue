ed332cf01ca4b2ce15e37c1313883afa83a76c36
Brian Mock <brian@wavebeem.com>
2021-12-23 12:08:14 -0800

    source maps please

--- stencil.config.ts
+++ stencil.config.ts
@@ -6,12 +6,13 @@ export const config: Config = {
   globalStyle: "src/global/app.css",
   globalScript: "src/global/app.ts",
   taskQueue: "async",
+  sourceMap: true,
   outputTargets: [
     {
       type: "www",
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: "https://myapp.local/",
+      baseUrl: "https://determined-mccarthy-f892a2.netlify.app/",
     },
   ],
 };
